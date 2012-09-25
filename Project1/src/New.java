00b1be2fd3acbfc6e1f3e83f77650aa33d03b721
20080104004 <20080104004@muhlab28.lab.khas.edu.tr>
2012-09-25 07:46:24 -0430

    title
    more description

---
+++ Project1/src/New.java
@@ -0,0 +1,14 @@
+
+public class New {
+ public static void main(String arg[]){
+ 
+	 System.out.println(mult(3,5));
+ 
+ }
+
+ public static int mult(int a, int b){
+int c = 0;
+ c = a * b;
+ return c;
+ }
+}
